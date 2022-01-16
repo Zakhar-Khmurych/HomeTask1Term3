ec5d6cb7ab28d4dff775fb7d0e2570ddd3f84f56
Zakhar-Khmurych <68910550+Zakhar-Khmurych@users.noreply.github.com>
2022-01-16 17:41:14 +0200

    Update Class.cs

--- Class.cs
+++ Class.cs
@@ -1,1 +1,25 @@
-//Console.Writeline("Hello World")
+using System;
+
+namespace ♂Gym♂
+{ 
+  public class ♂Leatherman♂ 
+  {
+    
+    private int ♂bucks♂ = 300;
+    public in ♂fucking_slaves♂;
+    
+    public void ♂Fisting♂(♂bucks♂)
+    {
+      if (♂fucking_slaves♂ > 0)
+      {
+        ♂bucks♂ += 300;
+      }
+      else
+      {
+        throw new Exception("♂Oh shit, I am sorry♂");
+      }
+      return ♂bucks♂;
+    }
+    
+  }
+}
